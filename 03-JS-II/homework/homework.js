54c779b327d81e408546b4d6f797adf5dd245284
pablosecuen@gmail.com <pablosecuen@gmail.com>
2022-09-29 20:20:52 -0300

    aplicados cambios de sintaxis para que el homework este correcto

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -105,7 +105,11 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
-  return numero % 1 ===0;
+  if (numero % 1 ===0){
+    return true
+  }else{
+    return false
+  }
 }
 
 function fizzBuzz(numero) {
